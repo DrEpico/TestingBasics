0c1040ed842c1505671db58e3338f3cd7f0dcc70
ali <adadashipour@gmail.com>
2024-08-07 14:29:10 +0100

    Add capitalise() and test for it

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 function getOne(){
     return 1;
 }
-
 console.log(getOne());
 
-module.exports = getOne;
+function capitalise(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function reverseString(string){
+
+}
+
+
+module.exports = { getOne, capitalise, reverseString };
--- script.test.js
+++ script.test.js
@@ -1,7 +1,23 @@
-const getOne = require('./script.js');
+const assignment = require('./script.js');
 
 describe('get one', () => {
     test('gets one', () => {
-        expect(getOne()).toBe(1);
+        expect(assignment.getOne()).toBe(1);
     });
+});
+
+describe('capitalise', () => {
+    test('Should return the parameter with the first character capitalized.', () => {
+        expect(assignment.capitalise('word')).toBe('Word');
+    });
+
+    test('Should return the same string if the first character is already capitalized.', () => {
+        expect(assignment.capitalise('Word')).toBe('Word');
+    });
+
+    test('Should handle an empty string by returning an empty string.', () => {
+        expect(assignment.capitalise('')).toBe('');
+    });
+
+    //etc.
 });
